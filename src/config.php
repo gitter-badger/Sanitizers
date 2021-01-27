3a7fccc4d88daef5184e74be8a5ca67b903b1bae
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2021-01-27 21:34:34 +0400

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,6 +1,9 @@
 <?php
 $config = [
-    "maxInputLength" => 80
+    "maxInputLength" => 80, // Maximum length of string (user input)
+// if more than length then extra characters will be removed
+    "encoding" => "UTF-8" // See https://www.php.net/manual/en/function.htmlspecialchars
+// For a list of encoding options
 ];
 
 ?>
